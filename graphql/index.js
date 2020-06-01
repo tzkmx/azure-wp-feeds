b832ca68d9bd3d68c80e9614dc20978d3b2ad6c5
Jesus E. Franco Martinez <tezcatl@fedoraproject.org>
2020-06-01 01:54:25 -0500

    hello graphql

---
+++ graphql/index.js
@@ -0,0 +1,17 @@
+﻿const { ApolloServer, gql } = require('apollo-server-azure-functions')
+
+const typeDefs = gql`
+    type Query {
+        hello: String!
+    }
+`
+
+const resolvers = {
+  Query: {
+    hello: () => 'hello World!'
+  }
+}
+
+const server = new ApolloServer({ typeDefs, resolvers })
+
+exports.graphqlHandler = server.createHandler()
