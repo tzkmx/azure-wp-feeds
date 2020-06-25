a7d7bbe6949bb5621c7568b0fa9914038ab05f35
Jesus E. Franco Martinez <tezcatl@fedoraproject.org>
2020-06-25 12:20:17 -0500

    fix order imagenes

--- graphql/imageDataLoader.js
+++ graphql/imageDataLoader.js
@@ -11,7 +11,7 @@ function imageDataLoader (domain) {
   const batchLoaderFn = keys => {
     const ids = keys.join(',')
 
-    return fetch(`${domain}/wp-json/wp/v2/media/?include=${ids}`)
+    return fetch(`${domain}/wp-json/wp/v2/media/?orderby=include&include=${ids}`)
       .then(res => res.json())
   }
   const loader = new DataLoader(batchLoaderFn)
